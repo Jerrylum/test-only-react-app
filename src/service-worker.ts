bc394eca452e1ca4181ab16c0b3632dc17b56592
Jerrylum <serverofjerry@gmail.com>
2023-06-17 03:23:41 +0800

    :art: Modify sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -57,13 +57,13 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin,
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: 'resources',
+    cacheName: 'images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used resources are removed.
+      // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
     ],
   })

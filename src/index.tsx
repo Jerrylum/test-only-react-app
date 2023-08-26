e2abd23e3d9309107f83cfe2251d547750078d54
Jerrylum <serverofjerry@gmail.com>
2023-08-27 04:56:27 +0800

    :art: Add hello

--- src/index.tsx
+++ src/index.tsx
@@ -23,3 +23,5 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// hello
